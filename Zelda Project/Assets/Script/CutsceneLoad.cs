f8cc08eb9cee3908522c334e632bd0c21d3b9e05
Honnyedson Cruz <Honnyedson.c@escolar.ifrn.edu.br>
2023-12-16 16:28:21 -0300

    Melhorias

---
+++ Zelda Project/Assets/Script/CutsceneLoad.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneLoad : MonoBehaviour
+{
+    private Player playerCutscene;
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerCutscene = FindObjectOfType<Player>();
+    }
+    
+    void LateUpdate()
+    {
+        if (playerCutscene.trocou == true)
+        {
+            StartCoroutine("PlayCutscene");
+        }
+    }
+
+    IEnumerator PlayCutscene()
+    {
+        playerCutscene.speed = 0f;
+        anim.SetBool("Activation", true);
+        playerCutscene.animator.SetInteger("Transition", 0);
+        yield return new WaitForSeconds(0.8f);
+        playerCutscene.walkLiberado = false;
+        anim.SetBool("Activation", false);
+        playerCutscene.trocou = false;
+        yield return new WaitForSeconds(0.8f);
+        playerCutscene.walkLiberado = true;
+        playerCutscene.speed = 5f;
+    }
+}
